bfca011236069256f1648f8f3f3cfae3edcb1e5e
FacundoRamirez99 <68912078+FacundoRamirez99@users.noreply.github.com>
2020-09-13 23:50:37 -0300

    Comentarios e info de producto
    agregada caja de comentarios,  comentarios de JSON. funcion de publicar comentario y rating, informacion del producto. + pequeños cambios en nombre de usuario de nav bar

--- js/login.js
+++ js/login.js
@@ -1,6 +1,15 @@
+let nombreUsuario = "";
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
+
+function setName(){
+    nombreUsuario = document.getElementById("nombreUsuarioNav").value;
+    if(nombreUsuario!=undefined && nombreUsuario!=""){
+        localStorage.setItem("usuario",nombreUsuario);
+    }
+}
+
--- js/product-info.js
+++ js/product-info.js
@@ -1,4 +1,8 @@
 var product = {};
+var titles = [];
+var date = new Date();
+var titleInput = document.getElementById("title");
+var messageBox = document.getElementById("display");
 function showImagesGallery(array){
 
     let htmlContentToAppend = "";
@@ -18,6 +22,24 @@ function showImagesGallery(array){
     }
 }
 
+
+function showRaiting(){
+    let score = videogame.rating;
+    let stars ="";
+    let html = "";
+    for(let i = 1; i<=maxRanking;i++){
+     if (i<=score) {
+           stars+='<i class="fa fa-star checked"></i>';
+    } else {
+        stars += '<i class="fa fa-star"></i>'
+    }
+}
+
+    html = `<span>  ${stars}</span>`
+
+    document.getElementById("rating").innerHTML = html;
+
+}
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
@@ -43,6 +65,65 @@ document.addEventListener("DOMContentLoaded", function(e){
 
             //Muestro las imagenes en forma de galería
             showImagesGallery(product.images);
+            
+        }
+    });
+});
+
+//Caja de comentarios
+function insert () {
+titles.push(titleInput.value);
+clearAndShow();
+}
+
+function clearAndShow (){
+    var personalScore = document.getElementById("score").value;
+   
+    
+titleInput.value = "";
+messageBox.innerHTML = "";
+messageBox.innerHTML +=`
+<div class="col-lg-3 col-md-4 col-6">
+    <div class="d-block mb-4 h-100">
+        <p class="nombreUsuarioComentario">`+localStorage.getItem("usuario")+`</p>
+        <p>`+ titles.join("<br/> ") +`</p>
+        <p class="text-muted">`+ date.toDateString() +`  -   Puntuación:  `+personalScore+` </p>
+        <hr class="my-3">
+    </div>
+</div>
+`
+}
+
+//mostrar comentarios de json
+
+function comentariosJson(commentArray){
+
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < commentArray.length; i++){
+        let comentario = commentArray[i];
+
+        htmlContentToAppend += `
+        <div class="col-lg-3 col-md-4 col-6">
+            <div class="d-block mb-4 h-100">
+                <p style="color:lightseagreen">`+ comentario.user+`</p>
+                <p>`+ comentario.description+`</p>
+                <p class="text-muted">`+ comentario.dateTime+`  -   Puntuación:  `+comentario.score+` </p>
+                <hr class="my-3">
+            </div>
+        </div>
+        `
+
+        document.getElementById("comentariosJson").innerHTML = htmlContentToAppend;
+    }
+}
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            productComments=resultObj.data;
+            comentariosJson(productComments);
+            showRaiting();
+        
         }
     });
 });
