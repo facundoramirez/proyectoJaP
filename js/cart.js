cf8fab8bcf4cf64eac93f0c6033b0465735a69c2
FacundoRamirez99 <68912078+FacundoRamirez99@users.noreply.github.com>
2020-10-21 11:45:41 -0300

    actualizacion de carrito
    funcion de restar articulo borrado al total pendiente

--- js/cart.js
+++ js/cart.js
@@ -2,6 +2,7 @@ let arrayArticles = [];
 let subtotal = 0;
 var total = 0;
 var totalEnvio=0;
+var subtotalDelBorrado=0;
 
 
 function calculoSubtotal(count, index) {// devuelve el subtotal del articulo en pesos
@@ -20,9 +21,13 @@ function addEventCount() { //funcion que contiene el addeventlistener para detec
         subtotalArray[i].addEventListener("change", function () {//detecta el cambio con "change"
             document.getElementById("productSubtotal-" + i).innerHTML = arrayArticles[i].currency + " " + subtotalArray[i].value * arrayArticles[i].unitCost;//actualiza el subtotal del articulo
             updateAllSubtotal();//actualiza el subtotal del tablefoot
+            clearRadioBtn();
+            ocultarBtnModal();
         });
     }
 }
+
+
 
 function updateAllSubtotal() { //funcion para mostrar y onchange actualizar el subtotal del final de pagina 
     let subtotalArray = document.getElementsByClassName("subtotalArticle");
@@ -47,19 +52,46 @@ function mostrarArticulos(articles) { //Crea una fila de tabla por cada articulo
         productCurrency = articles[i].currency;
 
         htmlContentToAppend += `
-        <tr>
+        <tr id="articleRow${[i]}">
       <td><img src="`+ articles[i].src + `" width="50px"> </td> 
       <td>`+ articles[i].name + `</td>
       <td>`+ articles[i].currency + " " + articles[i].unitCost + `</td>
       <td><input class="form-control subtotalArticle" style="width: 60px;" type="number" id="productCount-${i}" value="` + articles[i].count + `"min="1"></td>
       
-      <td><span id="productSubtotal-${i}" style="font-weight: bold;">${articles[i].currency}${articles[i].unitCost * articles[i].count}</span></td>
+      <td><span id="productSubtotal-${i}" style="font-weight: bold;" value="${articles[i].unitCost * articles[i].count}">${articles[i].currency}${articles[i].unitCost * articles[i].count}</span></td>
+      <td style="padding-left: 1.75rem;"><input class="eliminar" id="eliminar${[i]}" type="image" src="png/delete-3x.png" /> </td>
     </tr> `
-
-    }
+    
+    } 
     document.getElementById("cartProducts").innerHTML = htmlContentToAppend;//escribe en el html
     addEventCount(); //funcion que contiene el addeventlistener
     updateAllSubtotal(); //actualiza el subtotal y el total en el tablefoot
+    
+  
+      
+
+}
+
+function borrarArticulo(){
+    let trash = document.getElementsByClassName("eliminar");
+    for (let i=0; i<trash.length; i++){
+        trash[i].addEventListener("click",function(){
+            var element = document.getElementById("articleRow"+[i]);
+            element.classList.add("d-none");
+
+            restaDeElementoBorrado(i);
+            clearRadioBtn();
+            ocultarBtnModal();
+        });
+    }
+}
+
+function restaDeElementoBorrado(i){
+    var resta = 0;
+    subtotalDelBorrado = document.getElementById("productSubtotal-"+i).value;
+    resta = total - subtotalDelBorrado;
+    document.getElementById("subtotalText").innerText ="UYU " + resta;
+    document.getElementById("totalText").innerText ="UYU " + resta;
 }
 
 
@@ -71,35 +103,37 @@ numTarjeta.addEventListener('keypress', onkeyPress);
 numTarjeta.addEventListener('keydown', onkeyPress);
 numTarjeta.addEventListener('keyup', onkeyPress);
 
-function paymentMethod() {
-    const rbs = document.querySelectorAll('input[name="payOpt"]');
+function sentMethod() { // Funcion que se ejecuta al presionar el botón de comprar (fuera del modal)
+    const radioBtns = document.querySelectorAll('input[name="payOpt"]');
     let modalTitle = "";
     let selectedValue;
     let modalTotalYEnvio = 0;
     
-    for (const rb of rbs) {
+    for (const rb of radioBtns) {
         if (rb.checked) {
             selectedValue = rb.value;
             break;
         }
     }
-    modalTitle += `Pago<b> ${selectedValue}</b> seleccionado.`
-    modalTotalYEnvio = `Total a pagar: UYU${totalEnvio}.`
+    modalTitle += `Envío<b> ${selectedValue}</b> seleccionado.`//custom text del modal
+    modalTotalYEnvio = `Total a pagar: UYU${totalEnvio}.`// total reflejado en el modal
     document.getElementById("selectedPaymentTitle").innerHTML = modalTitle;
+    document.getElementById("AddressTitle").innerHTML = modalTitle;
     document.getElementById("totalModal").innerHTML = modalTotalYEnvio;
+
     totalMasEnvio(selectedValue);
     
 };
 
-function addEventRadio(){
-    let contact = document.querySelectorAll('input[name="payOpt"]');
-    if( contact ){
-      for( let i = 0; i < contact.length; i++ ){
-        contact[i].addEventListener("click", function(){
-          let item = this.value; // this == the clicked radio, which launched the function
+function addEventRadio(){//Cada vez que se clickea en un boton de radio actualiza el total + el costo de envio
+    let opciones = document.querySelectorAll('input[name="payOpt"]');
+    if( opciones ){
+      for( let i = 0; i < opciones.length; i++ ){
+        opciones[i].addEventListener("click", function(){
+          let item = this.value; // devuelve el value del radio button seleccionado
           
-          totalMasEnvio(item);
-          document.getElementById("totalText").innerHTML= "UYU " + totalEnvio;
+          totalMasEnvio(item);//toma el value del radio como parametro y actualizar el total+ envio
+          document.getElementById("totalText").innerHTML= "UYU " + totalEnvio;//escribe el total + envio
           
           
         });
@@ -107,32 +141,69 @@ function addEventRadio(){
     }
 }
 
-function totalMasEnvio(rbs) {
-    if (rbs === "premium") {
+function totalMasEnvio(seleccion) {//funcion que calcula el tipo de envio mas el total
+    if (seleccion === "premium") {
         totalEnvio = (total * 15) / 100 + total;
     } 
 
-    if (rbs === "express") {
+    if (seleccion === "express") {
         totalEnvio = (total * 7) / 100 + total;
     }
 
-    if (rbs === "standard") {
+    if (seleccion === "standard") {
         totalEnvio = (total * 5) / 100 + total;
     }
 }
 
-function clearRadioBtn(){
+function clearRadioBtn(){// funcion que se ejecuta al cargar el documento y deselecciona las opciones de radio
     var ele = document.getElementsByClassName("radioevent");
    for(var i=0;i<ele.length;i++)
       ele[i].checked = false;
 }
 
-function mostrarBtnModal(){
+function mostrarBtnModal(){//funcion que se ejecuta al presionar un boton de radio y muestra el boton para continuar con la compra
     var element = document.getElementById("buyBtn");
-  element.classList.remove("d-none");
+  element.classList.remove("d-none");//elimina la clase que oculta al boton
+}
+function ocultarBtnModal(){//funcion que oculta el boton de compra
+    var element = document.getElementById("buyBtn");
+    element.classList.add("d-none");//agrega la clase que oculta al boton
 }
 
-function validar() {
+function paymentMethod(){
+
+    if(document.getElementById("cardCheck").checked){
+        return validarTarjeta();
+    }if(document.getElementById("depositCheck").checked){
+       return validarDeposito();
+    }
+}
+
+function validarDeposito(){
+    var elemento = document.getElementById("numDeposito").value
+
+    if (elemento ==""){
+        let dangerAlert = `<div class="alert alert-danger" role="alert">
+        ¡Debes ingresar el número de cuenta!
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close" style="width: min-content">
+         <span aria-hidden="true">&times;</span>
+       </button>
+      </div>`
+        document.getElementById("modalAlertRow").innerHTML+= dangerAlert;
+         return false
+       }else {
+           let succedAlert = `<div class="alert alert-success" role="alert">
+           Compra realizada con éxito :)
+           <button type="button" class="close" data-dismiss="alert" aria-label="Close" style="width: min-content">
+            <span aria-hidden="true">&times;</span>
+          </button>
+         </div>`
+         document.getElementById("modalAlertRow").innerHTML+= succedAlert;
+         return false
+    }
+}
+
+function validarTarjeta() {//funcion que verifica si cada input de la tarjeta está escrito
     var elemento1 = document.getElementById("numTarjeta").value
     var elemento2 = document.getElementById("mesVenc").value
     var elemento3 = document.getElementById("anioVenc").value
@@ -146,7 +217,7 @@ function validar() {
   </button>
  </div>`
    document.getElementById("modalAlertRow").innerHTML+= dangerAlert;
-    return false
+    
   }else {
       let succedAlert = `<div class="alert alert-success" role="alert">
       Compra realizada con éxito :)
@@ -155,8 +226,28 @@ function validar() {
      </button>
     </div>`
     document.getElementById("modalAlertRow").innerHTML+= succedAlert;
-    return false
+    
   }
+}
+
+function validarDireccion(){
+    var elemento1 = document.getElementById("calleInput").value
+    var elemento2 = document.getElementById("esquinaInput").value
+    var elemento3 = document.getElementById("numInput").value
+    var botonSiguiente = document.getElementById("nextModal");
+    if (elemento1 == "" || elemento2 == "" || elemento3 == ""){
+        botonSiguiente.disabled = true;
+       }else {
+        botonSiguiente.disabled = false;
+       }
+}
+
+function addEventEnvioEnModal(){
+    document.querySelectorAll('.addressInput').forEach(item => {
+        item.addEventListener('change', event => {
+          validarDireccion();
+        })
+      })
 }
 
 
@@ -168,6 +259,9 @@ document.addEventListener("DOMContentLoaded", function (e) {
             mostrarArticulos(arrayArticles);
             addEventRadio();
             clearRadioBtn();
+            addEventEnvioEnModal()
+            validarDireccion();
+            borrarArticulo()
         }
     })
 });
